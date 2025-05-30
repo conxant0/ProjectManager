8f056ffc54024a606e061d150ec2450cd01f91c6
conxant0 <151835239+conxant0@users.noreply.github.com>
2025-05-30 16:27:05 +0800

    fixed dashboard

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Dashboard.css'
 import supabase from '../../helper/supabaseClient'
-<<<<<<< HEAD
 
 const sampleProjects = [
   {
@@ -14,8 +13,6 @@ const sampleProjects = [
   },
   // Add more sample projects if needed
 ]
-=======
->>>>>>> 0dd64c04cf7e81e25557d6cde46725362038560f
 
 const Dashboard = () => {
   const navigate = useNavigate()
@@ -24,17 +21,6 @@ const Dashboard = () => {
   const [selectedProject, setSelectedProject] = useState(null)
 
   const handleLogout = async () => {
-<<<<<<< HEAD
-    const { error } = await supabase.auth.signOut()
-  if (error) {
-    console.error('Logout failed:', error.message)
-  } else {
-    navigate('/') // redirect to home/login after logout
-    }
-  }
-  
-  const goToProfile = () => navigate('/profile')
-=======
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error('Error logging out:', error.message);
@@ -47,7 +33,6 @@ const Dashboard = () => {
     navigate('/profile')
   }
 
->>>>>>> 0dd64c04cf7e81e25557d6cde46725362038560f
   const goToAddProject = () => {
     if (editorMode) navigate('/add-project')
   }
